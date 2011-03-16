2185fe98b47dafa88e5fb29b4da83d2447908981
Cyrus Harmon <ch-lisp@bobobeach.com>
2011-03-16 09:50:04 -0700

    add opticl-test.asd and update some of the test forms

---
+++ opticl-test.asd
@@ -0,0 +1,14 @@
+
+(asdf:defsystem :opticl-test
+  :name "opticl-test"
+  :author "Cyrus Harmon <ch-lisp@bobobeach.com>"
+  :licence "BSD"
+  :default-component-class cl-source-file
+  :depends-on (opticl)
+  :serial t
+  :components
+  ((:file "package")
+   (:file "opticl-test")
+   (:file "shapes-test")
+   (:file "transform-test")
+   (:file "gamma-test")))
--- opticl-test.lisp
+++ opticl-test.lisp
@@ -60,15 +60,15 @@
     (let ((out (output-image "truck.jpeg")))
       (write-jpeg-file out img))))
 
-
+;;; TIFF Files
 (defun test-snow ()
-  (let* ((file (tiff-image "snow.tiff"))
+  (let* ((file (test-image "snow.tiff"))
          (img (read-tiff-file file)))
-    (let ((out (output-image "snow-rgba.jpeg")))
+    (let ((out (output-image "snow-rgb.jpeg")))
       (write-jpeg-file out img))))
 
 (defun test-snow-lzw ()
-  (let* ((file (tiff-image "snow-lzw.tiff"))
+  (let* ((file (test-image "snow-lzw.tiff"))
          (img (read-tiff-file file)))
     (let ((out (output-image "snow-lzw.jpeg")))
       (write-jpeg-file out img))))
@@ -85,6 +85,88 @@
     (let ((out (output-image "snow-packbits.jpeg")))
       (write-jpeg-file out img))))
 
+(defun test-tiff-write-8-bit-gray ()
+  (let* ((file (test-image "tower-gray.jpg"))
+         (img (read-jpeg-file file)))
+    (let ((out (output-image "tower-gray.tiff")))
+      (write-tiff-file out img))))
+
+(defun test-tiff-write-8-bit-rgb ()
+  (let* ((file (test-image "truck.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "truck.tiff")))
+      (write-tiff-file out img))))
+
+(defun test-tiff-write-8-bit-rgba ()
+  (let* ((file (test-image "truck-alpha.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "truck-alpha.tiff")))
+      (write-tiff-file out img))))
+
+(defun test-tiff-write-16-bit-rgb ()
+  (let* ((file (test-image "truck-16.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "truck-16.tiff")))
+      (write-tiff-file out img))))
+
+(defun test-tiff-write-8-bit-rgb-little-endian ()
+  (let* ((file (test-image "truck.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "truck-little-endian.tiff")))
+      (write-tiff-file out img :byte-order :little-endian))))
+
+(defun test-tiff-write-8-bit-rgb-read-lzw ()
+  (let* ((file (test-image "truck-lzw.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "truck.tiff")))
+      (write-tiff-file out img))))
+
+(defun test-coerce-image-to-grayscale ()
+  (let* ((file (test-image "truck.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "truck-gray.jpeg")))
+      (write-image-file out (coerce-image img 'gray-image)))))
+
+(defun test-coerce-image-to-grayscale-luminance ()
+  (let* ((file (test-image "truck.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "truck-lum.jpeg")))
+      (write-image-file out (coerce-image img 'gray-image :preserve-luminance t)))))
+
+(defun test-set-region-pixels ()
+  (let* ((file (test-image "truck.tiff"))
+         (img (read-image-file file)))
+    (let ((out (output-image "truck-inv.tiff")))
+      (set-region-pixels (i j 75 75 300 300)
+          img
+        (values-list (map 'list (lambda (x) (- 255 x)) (pixel* img i j))) )
+      (write-image-file out img))))
+
+(defun test-set-pixels ()
+  (declare (optimize (debug 3)))
+  (let* ((file (test-image "salad.jpg"))
+         (img (read-image-file file)))
+    (let ((out (output-image "salad-swap.tiff")))
+      (set-pixels (i j)
+          img
+        (multiple-value-bind (r g b)
+            (pixel img i j)
+          (values b (floor (* g 0.8)) r)))
+      (write-image-file out img))))
+
+(defun test-tiff-read-indexed ()
+  (let* ((file (test-image "truck-indexed.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "truck-from-index.png")))
+      (write-image-file out img))))
+
+(defun test-tiff-read-1-bit ()
+  (let* ((file (test-image "goat.tiff"))
+         (img (read-tiff-file file)))
+    (let ((out (output-image "goat.png")))
+      (write-image-file out (coerce-image img '8-bit-gray-image)))))
+
+;;; PNM Files
 (defun test-pgm-read-binary ()
   (let* ((file (test-image "tower-gray-binary.pgm"))
          (img (read-pgm-file file)))
@@ -131,4 +213,32 @@
          (img (read-png-file file)))
     (let ((out (output-image "truck-binary.ppm")))
       (write-ppm-file out img))))
+
+;;;
+
+(defmacro foo () 
+  `(make-8-bit-gray-image 4 4 :initial-element 32))
+
+(let ((moose))
+  (setf (pixel (setf moose (foo)) 0 0) 4)
+  moose)
+
+(setf (pixel (foo) 0 0) 4)
+
+(setf (aref (make-8-bit-gray-image 4 4 :initial-element 32) 0 0) 4)
+
+;;;
+
+(defun test-gif-read ()
+  (let* ((file (test-image "mushroom.png"))
+         (img (read-image-file file)))
+    (let ((out (output-image "mushroom-out.gif")))
+      (write-image-file out img))))
+
+
+(let* ((file (test-image "truck.tiff"))
+         (img (read-image-file file)))
+    (let ((out (output-image "truck.jpeg")))
+      (write-image-file out img)))
+
 
