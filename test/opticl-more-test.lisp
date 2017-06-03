424f8abe430468989584b5fa422801a161d428e2
Cyrus Harmon <cyrus@bobobeach.com>
2017-06-03 10:01:57 -0700

    write tiff as TIFF, not JPEG

--- test/opticl-more-test.lisp
+++ test/opticl-more-test.lisp
@@ -47,7 +47,7 @@
          (img (read-png-file file)))
     ;; for now just write the image out. dont' check it.
     (let ((out (output-image "horse-8-bit-from-4-bit-rgb.tiff")))
-      (is (equal out (write-jpeg-file out img))))))
+      (is (equal out (write-tiff-file out img))))))
 
 (test png-read-16-bit-rgb
   (let* ((file (test-image "horse-16-bit-rgb.png"))
