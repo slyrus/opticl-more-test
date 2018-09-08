a26ead03fa52dfc785828d23028fe8707e4cbcf8
Cyrus Harmon <cyrus@bobobeach.com>
2018-09-08 11:14:00 -0700

    fix HSV symbol usage and add 16-bit HSV example

--- hsv-stuff.lisp
+++ hsv-stuff.lisp
@@ -7,6 +7,11 @@
    (read-tiff-file (test-image "truck.tiff"))
    'opticl::hsv-image))
 
+(defparameter *truck-hsv-16*
+  (opticl:coerce-image
+   (read-tiff-file (test-image "truck-16.tiff"))
+   'opticl::hsv-image))
+
 (let ((out (output-image "truck-round-trip.jpeg")))
   (opticl:write-jpeg-file
    out
@@ -14,28 +19,36 @@
 
 (defun rotate-hsv-image (image theta)
   (locally
-      (declare (type opticl-core::hsv-image image))
+      (declare (type opticl:hsv-image image))
     (opticl:with-image-bounds (y x)
         image
-      (let* ((rot-image (opticl-core::make-hsv-image y x)))
-        (declare (type opticl-core::hsv-image rot-image))
+      (let* ((rot-image (opticl:make-hsv-image y x)))
+        (declare (type opticl:hsv-image rot-image))
         (opticl:do-pixels (i j)
             image
           (let ((hsv-pixel (opticl:pixel image i j)))
-            (let ((h (opticl-core::hsv-pixel-hue hsv-pixel))
-                  (s (opticl-core::hsv-pixel-saturation hsv-pixel))
-                  (v (opticl-core::hsv-pixel-value hsv-pixel)))
+            (let ((h (opticl:hsv-pixel-hue hsv-pixel))
+                  (s (opticl:hsv-pixel-saturation hsv-pixel))
+                  (v (opticl:hsv-pixel-value hsv-pixel)))
               (setf (opticl:pixel rot-image i j)
-                    (opticl-core::make-hsv-pixel :hue (mod (+ h theta) 360)
+                    (opticl:make-hsv-pixel :hue (mod (+ h theta) 360)
                                                  :saturation s
                                                  :value v)))))
         rot-image))))
 
 (defparameter *truck-hsv-rot*
-  (rotate-hsv-image *truck-hsv* 210))
+  (rotate-hsv-image *truck-hsv* 100))
 
 (let ((out (output-image "truck-rot.jpeg")))
   (opticl:write-jpeg-file
    out
    (opticl:coerce-image *truck-hsv-rot* 'opticl:8-bit-rgb-image)))
 
+
+(defparameter *truck-hsv-16-rot*
+  (rotate-hsv-image *truck-hsv-16* 40))
+
+(let ((out (output-image "truck-16-rot.tiff")))
+  (opticl:write-tiff-file
+   out
+   (opticl:coerce-image *truck-hsv-rot* 'opticl:16-bit-rgb-image)))
